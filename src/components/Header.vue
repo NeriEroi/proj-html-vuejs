ad941c8131575d4b52691840410e438295e2311a
Michele <micheleierniero@gmail.com>
2021-09-30 12:25:24 +0200

    header, oggetto in data( ) e ciclo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,48 @@
   <header>
     <!-- <img src="../assets/images/driving_preview.jpg" alt=""> -->
 
-      
+    <ul v-for="(headerLink, index) in headerLinks" :key="index">
+      <li>
+        {{ headerLink.text }}
+      </li>
+    </ul>
+
+    <div class="button_cont">
+      <div class="button">
+        BOOK KNOW
+      </div>
+    </div>
+    
   </header>
 </template>
 
 <script>
 export default {
     name: 'Header',
+    data() {
+      return {
+        headerLinks: [
+          {
+            text: "home"
+          },
+          {
+            text: "about"
+          },
+          {
+            text: "prices"
+          },
+          {
+            text: "courses"
+          },
+          {
+            text: "locations"
+          },
+          {
+            text: "blog"
+          },
+        ]
+      }
+    }
 }
 </script>
 
