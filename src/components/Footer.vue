9103a892040d926e5072894b46e28f0dd8b470a0
Michele <micheleierniero@gmail.com>
2021-09-30 11:18:09 +0200

    struttura

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <!-- da compilare  -->
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
