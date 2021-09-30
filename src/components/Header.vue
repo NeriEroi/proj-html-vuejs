088da2c6d6b856c44cdce20460abac78ce171c58
Michele <micheleierniero@gmail.com>
2021-09-30 13:17:34 +0200

    header, sub_header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,23 @@
   <header>
     <!-- <img src="../assets/images/driving_preview.jpg" alt=""> -->
 
+    <div class="sub_header">
+
+      <section>
+        <h5>
+          We have a 95% Succssful Pass Rate!
+        </h5>
+      </section>
+
+      <section>
+        <h5>
+          <i class="fas fa-phone"></i>
+          Give us a call to book your tuition! 1-800-555-555
+        </h5>
+      </section>
+
+    </div>
+
     <ul>
       <li v-for="(headerLink, index) in headerLinks" :key="index">
         <a :class="(headerLink.status) ? 'new' : null" href="#">
@@ -56,14 +73,47 @@ export default {
 <style scoped lang="scss">
 @import "../style/generals";
 @import "../style/vars";
+@import '~@fortawesome/fontawesome-free/css/all.min.css';
 
 header {
+  width: 100%;
+
+  .sub_header {
+    height: 35px;
+    background-color: $headergray;
+    display: flex;
+
+    section {
+      height: 100%;
+      width: calc(100% / 2);
+      // background-color: magenta;
+      // border: 1px solid black;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      h5 {
+        color: $gray;
+        font-size: 10px;
+
+        i {
+          margin-right: 10px;
+        }
+      }
+    }
+  }
+
   ul {
-    li .new::after {
+    li a {
+      color: lime;
+      text-decoration: none;
+
+      .new::after {
       width: 10px;
       height: 10px;
       background-color: lime;
       content: "new";
+      }
     }
   }
 }
