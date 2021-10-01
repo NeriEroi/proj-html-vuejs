a9eb65c6899603ebd5d963c0bf90c7b13368c343
Michele <micheleierniero@gmail.com>
2021-10-01 11:53:42 +0200

    footer, aggiunta dettagli e ciclo da modificare

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,56 +1,135 @@
 <template>
   <footer>
     
+    <!-- sezione superiore del footer  -->
     <section class="footer_top">
 
       <div class="box">
 
         <section>
+          
+          <div class="logo_cont">
+
+            <img src="../assets/images/avada-drivers-logo-1x.png" alt="">
+
+          </div>
+
+          <p>
+            Avada Driving School is the right choise for 
+            experienced drivers looking for a refresher
+            course or the brand new driver wanting to
+            pass first time.
+          </p>
 
         </section>
 
         <section>
 
+          <h1>
+            CONTACT DETAILS
+          </h1>
+
         </section>
 
+        <!-- COURSES -->
         <section>
+
+          <h1>
+            COURSES
+          </h1>
+
+          <ul>
+
+            <li v-for="(course, index) in courses" :key="index">
+                {{ course.icon }} {{ course.text }}
+            </li>
+
+          </ul>
+
 
         </section>
+        <!-- / COURSES -->
 
         <section>
+          
+          <div class="discount_cont">
+
+            <img src="../assets/images/footer-ad-grid.png" alt="discount footer grid">
+
+          </div>
+
+          <div class="book_now">
+            BOOK NOW
+          </div>
 
         </section>
 
       </div>
 
     </section>
+    <!-- / sezione superiore del footer  -->
 
+    <!-- sezione inferiore del footer  -->
     <section class="footer_bottom">
 
       <div class="box">
 
         <section>
 
+          <p>
+            <i class="far fa-copyright"></i> Copyright 2012 - 2020 | Avada Theme by <span>ThemeFusion</span> | All Rights Reserved| Powered by <span>WordPress</span>
+          </p>
+
         </section>
 
         <section>
+
+          <i class="fab fa-facebook-f"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-youtube"></i>
+          <i class="fab fa-instagram"></i>
 
         </section>
         
       </div>
       
     </section>
-
+    <!-- / sezione inferiore del footer  -->
   </footer>
 </template>
 
 <script>
 export default {
     name: 'Footer',
+    data() {
+      return {
+        courses: [
+          {
+            icon: "far fa-arrow-alt-circle-right",
+            text: "Pass Plus"
+          },
+          {
+            icon: "far fa-arrow-alt-circle-right",
+            text: "Intensive Course"
+          },
+          {
+            icon: "far fa-arrow-alt-circle-right",
+            text: "Automatic"
+          },
+          {
+            icon: "far fa-arrow-alt-circle-right",
+            text: "Instructor Training"
+          },
+        ]
+      }
+    }
 }
 </script>
 
 <style scoped lang="scss">
+@import "../style/generals";
+@import "../style/vars";
+@import '~@fortawesome/fontawesome-free/css/all.min.css';
 
 .footer_top {
   width: 100%;
@@ -62,6 +141,27 @@ export default {
     height: 100%;
     background-color: rgb(0, 255, 21);
     border: 1px solid black;
+
+    .discount_cont {
+      width: 210px;
+      height: 210px;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .book_now {
+      width: 50%;
+      height: 30px;
+      padding: 5px 15px;
+      margin-bottom: -20px;
+      border-radius: 10px;
+      background-color: $lightgreen;
+      color: white;
+      display: flex;
+      justify-content: center;
+    }
   }
 }
 .footer_bottom {
